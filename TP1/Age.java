cc8c1b190e11b2575dc23db63161298a2da3f480
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-02-13 17:56:47 +0000

    Finish TP1 class exercises

---
+++ TP1/Age.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.time.Year;
+
+public class Age {
+    final static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Find last year
+        int lastyear = Year.now().getValue() - 1;
+
+        System.out.print("Em que ano nasceste? ");
+        int birth = Integer.parseInt(in.nextLine());
+
+        System.out.print("Como te chamas? ");
+        String name = in.nextLine();
+
+        int diff = lastyear - birth;
+        System.out.printf("Olá, %s! Em 2022, fizeste %d anos.\n", name, diff);
+    }
+}
