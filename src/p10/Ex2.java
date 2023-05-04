11662dbe9c2abb9cef5cdd812a2d2965a03442c2
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-05-04 15:54:00 +0100

    P10, ex. 2: use TreeMap

--- src/p10/Ex2.java
+++ src/p10/Ex2.java
@@ -1,47 +1,45 @@
 package p10;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Ex2 {
 
-    private static Book randomizeBook(HashMap<GenderEnum, List<Book>> hashMap, GenderEnum key) {
+    private static Book randomizeBook(Map<GenderEnum, List<Book>> hashMap, GenderEnum key) {
         List<Book> list = hashMap.get(key);
         return list.get((int) (Math.random() * list.size()));
     }
 
     public static void main(String[] args) {
-        HashMap<GenderEnum, List<Book>> bookHashMap = new HashMap<>();
+        SortedMap<GenderEnum, List<Book>> bookTreeMap = new TreeMap<>();
 
-        bookHashMap.put(GenderEnum.DRAMA, new ArrayList<>());
-        bookHashMap.put(GenderEnum.FANTASY, new ArrayList<>());
-        bookHashMap.put(GenderEnum.SCIENCEFICTION, new ArrayList<>());
-        bookHashMap.put(GenderEnum.MYSTERY, new ArrayList<>());
-        bookHashMap.put(GenderEnum.HORROR, new ArrayList<>());
+        bookTreeMap.put(GenderEnum.DRAMA, new ArrayList<>());
+        bookTreeMap.put(GenderEnum.FANTASY, new ArrayList<>());
+        bookTreeMap.put(GenderEnum.SCIENCEFICTION, new ArrayList<>());
+        bookTreeMap.put(GenderEnum.MYSTERY, new ArrayList<>());
+        bookTreeMap.put(GenderEnum.HORROR, new ArrayList<>());
 
-        bookHashMap.get(GenderEnum.DRAMA).add(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925));
-        bookHashMap.get(GenderEnum.FANTASY).add(new Book("The Lord of the Rings", "J. R. R. Tolkien", 1954));
-        bookHashMap.get(GenderEnum.FANTASY).add(new Book("The Hobbit", "J. R. R. Tolkien", 1937));
-        bookHashMap.get(GenderEnum.SCIENCEFICTION).add(new Book("Dune", "Frank Herbert", 1965));
-        bookHashMap.get(GenderEnum.MYSTERY).add(new Book("The Maltese Falcon", "Dashiell Hammett", 1930));
-        bookHashMap.get(GenderEnum.HORROR).add(new Book("The Shining", "Stephen King", 1977));
+        bookTreeMap.get(GenderEnum.DRAMA).add(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925));
+        bookTreeMap.get(GenderEnum.FANTASY).add(new Book("The Lord of the Rings", "J. R. R. Tolkien", 1954));
+        bookTreeMap.get(GenderEnum.FANTASY).add(new Book("The Hobbit", "J. R. R. Tolkien", 1937));
+        bookTreeMap.get(GenderEnum.SCIENCEFICTION).add(new Book("Dune", "Frank Herbert", 1965));
+        bookTreeMap.get(GenderEnum.MYSTERY).add(new Book("The Maltese Falcon", "Dashiell Hammett", 1930));
+        bookTreeMap.get(GenderEnum.HORROR).add(new Book("The Shining", "Stephen King", 1977));
 
-        bookHashMap.get(GenderEnum.DRAMA).get(0).setYear(1926);
-        bookHashMap.get(GenderEnum.FANTASY).get(0).setTitle("The Hobbit");
-        bookHashMap.get(GenderEnum.SCIENCEFICTION).get(0).setAuthor("Frank Herbert");
+        bookTreeMap.get(GenderEnum.DRAMA).get(0).setYear(1926);
+        bookTreeMap.get(GenderEnum.FANTASY).get(0).setTitle("The Hobbit");
+        bookTreeMap.get(GenderEnum.SCIENCEFICTION).get(0).setAuthor("Frank Herbert");
 
-        bookHashMap.remove(GenderEnum.MYSTERY);
+        bookTreeMap.remove(GenderEnum.MYSTERY);
 
-        System.out.println(bookHashMap);
-        System.out.println(bookHashMap.keySet());
-        for (List<Book> list : bookHashMap.values()) {
+        System.out.println(bookTreeMap);
+        System.out.println(bookTreeMap.keySet());
+        for (List<Book> list : bookTreeMap.values()) {
             for (Book book : list) {
                 System.out.println(book);
             }
         }
 
-        System.out.println(randomizeBook(bookHashMap, GenderEnum.DRAMA));
-        System.out.println(randomizeBook(bookHashMap, GenderEnum.FANTASY));
+        System.out.println(randomizeBook(bookTreeMap, GenderEnum.DRAMA));
+        System.out.println(randomizeBook(bookTreeMap, GenderEnum.FANTASY));
     }
 }
