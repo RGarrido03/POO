605688dc0109a8ce879c114a896464c16b078fdd
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-05-11 15:26:07 +0100

    P11, ex. 3

---
+++ src/p11/ex3/Customer.java
@@ -0,0 +1,21 @@
+package p11.ex3;
+
+import java.util.List;
+
+public class Customer {
+    private final int customerId;
+    private final List<Double> meterReadings;
+
+    public Customer(int customerId, List<Double> meterReadings) {
+        this.customerId = customerId;
+        this.meterReadings = meterReadings;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public List<Double> getMeterReadings() {
+        return meterReadings;
+    }
+}
---
+++ src/p11/ex3/EnergyUsageReport.java
@@ -0,0 +1,65 @@
+package p11.ex3;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class EnergyUsageReport {
+    Map<Integer, Customer> customerMap = new TreeMap<>();
+
+    public void load(String path) throws FileNotFoundException {
+        Scanner input = new Scanner(new FileReader(path));
+        input.useDelimiter("[|\\n]");
+        int id;
+
+        while(input.hasNext()) {
+            id = Integer.parseInt(input.next());
+
+            customerMap.put(id,
+                new Customer(id, Arrays.asList(
+                    Double.parseDouble(input.next()),
+                    Double.parseDouble(input.next()),
+                    Double.parseDouble(input.next()),
+                    Double.parseDouble(input.next())
+                ))
+            );
+        }
+    }
+
+    public void addCustomer(Customer customer) {
+        customerMap.put(customer.getCustomerId(), customer);
+    }
+
+    public void removeCustomer(int id) {
+        customerMap.remove(id);
+    }
+
+    public Customer getCustomer(int id) {
+        return customerMap.get(id);
+    }
+
+    public double calculateTotalUsage(int id) {
+        double sum = 0;
+
+        for (double reading: customerMap.get(id).getMeterReadings()) {
+            sum += reading;
+        }
+
+        return sum;
+    }
+
+    public void generateReport(String path) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(path)) {
+            int id;
+
+            for (Customer customer: customerMap.values()) {
+                id = customer.getCustomerId();
+                out.printf("%d|%f", id, calculateTotalUsage(id));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Exiting...");
+            System.exit(1);
+        }
+    }
+}
